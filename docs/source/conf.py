af4559aa8282b799bb219a2c0342f11d3284549e
miladmozafari <milad.mozafari69@outlook.com>
2019-02-10 17:25:17 +0330

    katex for read the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,9 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
 
+import sphinxcontrib.katex
+import sphinxcontrib.katex as katex
+
 
 # -- Project information -----------------------------------------------------
 
